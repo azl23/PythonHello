9d819262ea930dcc0b069f1f8f61d1e9ea5e55ae
Olga <olgaharceva946@gmail.com>
2022-11-26 12:35:37 +0300

    Решение первой задачи на семинаре

---
+++ python/Seminar01/Ex01.py
@@ -0,0 +1,16 @@
+# Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+# *Примеры:*
+# - 5, 25 -> да
+# - 4, 16 -> да
+# - 25, 5 -> да
+# - 8,9 -> нет
+
+print('Введите первое число: ')
+number_one = int(input())
+print('Введите второе число: ')
+number_two = int(input())
+if number_one * number_one == number_two or number_two * number_two == number_one:
+    print('Да ')
+else:
+    print('Нет')
+
