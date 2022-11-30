4f24fda080039e87c692a4d3dcd42472d1b40e75
Olga <olgaharceva946@gmail.com>
2022-11-30 23:07:59 +0300

    Решение второй задачи

--- python/HomeWork02/Ex02.py
+++ python/HomeWork02/Ex02.py
@@ -2,3 +2,13 @@
 # Не используйте функцию math.factorial.
 # Пример:
 # - пусть N = 4, тогда [ 1, 2, 6, 24 ] #(1, 1*2, 1*2*3, 1*2*3*4)
+
+number = input('Введите число N:\n')
+if number.isdigit() and int(number)>0:
+    number = int(number)
+    result = [1]
+    for i in range(2,number+1):
+        result.append(result[i-2]*i)
+    print(f'N = {number}, Набор произведений: {result}')
+else:
+    print("Нужно ввести число больше 0")
